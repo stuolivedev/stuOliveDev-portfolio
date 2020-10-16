ee303c06806cc947306514ee651b01284e910bcd
Stu Olive <60480020+stuolivedev@users.noreply.github.com>
2020-10-16 16:39:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,36 @@ $(window).on('load',  function() {
     $('#portfolioContent').show();
     $('#aboutContent').hide();
     $('#contactContent').hide();
+
+    $('#portfolio').addClass('highlight');
 });
 
 $('#portfolio').on('click', function(){
     $('#portfolioContent').show();
     $('#aboutContent').hide();
     $('#contactContent').hide();
+
+    $('#portfolio').addClass('highlight');
+    $('#about').removeClass('highlight');
+    $('#contact').removeClass('highlight');
 });
 
 $('#about').on('click', function(){
     $('#portfolioContent').hide();
     $('#aboutContent').show();
     $('#contactContent').hide();
+
+    $('#portfolio').removeClass('highlight');
+    $('#about').addClass('highlight');
+    $('#contact').removeClass('highlight');
 });
 
 $('#contact').on('click', function(){
     $('#portfolioContent').hide();
     $('#aboutContent').hide();
     $('#contactContent').show();
+
+    $('#portfolio').removeClass('highlight');
+    $('#about').removeClass('highlight');
+    $('#contact').addClass('highlight');
 });
